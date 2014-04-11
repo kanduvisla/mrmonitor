58d0c970330f350c82ad46fa57b6c01879457833
Giel Berkers <info@gielberkers.com>
2014-04-11 22:11:52 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+    <title>Mr. Monitor</title>
+	<link rel="stylesheet" href="stylesheets/screen.css" />
+    <!-- Refresh every 5 minutes: -->
+    <meta http-equiv="refresh" content="300">
+</head>
+<body>
+<?php
+    if(file_exists('result.csv'))
+    {
+        // Get the number of lines:
+        $linecount = -1;
+        $file = fopen('result.csv', "r");
+        while(!feof($file)){
+          $line = fgets($file);
+          $linecount++;
+        }
+        fclose($file);
+
+        $file = fopen('result.csv', 'r');
+        $headers = fgetcsv($file);
+        $happy = true;
+        $error = false;
+        while($url = fgetcsv($file))
+        {
+            $info = array_combine($headers, $url);
+            $class = $info['success'] == '1' ? 'success' : 'error';
+            if($info['success'] != '1') {
+                $happy = false;
+                $error = true;
+            }
+            // Check for slow speed:
+            if($info['time_first'] > 0.5 || $info['time_total'] > 4)
+            {
+                $class .= ' slow';
+                $happy = false;
+            }
+            if($linecount > 25) { $class .= ' count50'; }
+            if($linecount > 50) { $class .= ' count100'; }
+            if($linecount > 100) { $class .= ' count200'; }
+            if($linecount > 200) { $class .= ' count400'; }
+            ?>
+                <div class="site <?php echo $class; ?>">
+                    <p class="url"><?php echo str_replace(array('http://', 'www.'), '', $info['url']); ?></p>
+                    <p class="code"><?php echo $info['code']; ?></p>
+                    <p class="time"><?php echo
+                            number_format($info['time_first'], 3, ',', '.'). '/' .
+                            number_format($info['time_total'], 3, ',', '.'); ?></p>
+                </div>
+            <?php
+        }
+        fclose($file);
+
+        if($happy)
+        {
+            ?>
+                <div id="happy">☺</div>
+            <?php
+        }
+
+        if($error)
+        {
+            ?>
+                <div id="error">
+					<audio src="error.mp3" autoplay="true"></audio>
+                </div>
+            <?php
+        }
+
+    }
+?>
+</body>
+</html>
---
+++ ping.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Ping a URL
+ *
+ * @param $url
+ * @return string|bool
+ */
+function ping($url)
+{
+    $url = strtolower($url);
+
+    // Get Curl information:
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    $info = curl_getinfo($ch);
+    $info = array(
+        'ip' => $info['primary_ip'],
+        'www' => substr($url, 0, 4) == 'www.',
+        'time_first' => $info['pretransfer_time'],
+        'time_total' => $info['total_time'],
+        'code' => $info['http_code'],
+        'size' => $info['size_download']
+    );
+
+    return $info;
+}
+
+/**
+ * Check URL
+ *
+ * @param $url
+ * @return array
+ */
+function check_url($url)
+{
+    $result = array(
+        'url' => $url,
+        'success' => 0,
+        'message' => ''
+    );
+
+    // Check if http://
+    if (substr($url, 0, 7) == 'http://') {
+        $url = str_replace('http://', '', $url);
+
+        // Check if www:
+        if (substr($url, 0, 4) == 'www.') {
+            $url = substr($url, 4);
+        }
+
+        // Check non-www:
+        $resultNonWww = ping($url);
+
+        // Check www:
+        $resultWithWww = ping('www.' . $url);
+
+        $redirectCodes = array(301, 302);
+
+        // Check for redirect:
+        if(
+            (in_array($resultWithWww['code'], $redirectCodes) && $resultNonWww['code'] == 200) ||
+            ($resultWithWww['code'] == 200 && in_array($resultNonWww['code'], $redirectCodes))
+        ) {
+            // Redirect is correct.
+            $siteResult = $resultWithWww['code'] == 200 ? $resultWithWww : $resultNonWww;
+
+            $result = array_merge($result, $siteResult);
+            $result['www'] = $result['www'] ? 1 : 0;
+
+            // Check size:
+            if($siteResult['size'] < 1024) {
+                $result['message'] = 'Site is less than 1kb. Something must be wrong!';
+            } else {
+                $result['success'] = 1;
+                $result['message'] = 'Ok';
+            }
+        } else {
+            $result['message'] = 'Www and non/www don\'t return 200/301/302';
+        }
+    } else {
+        $result['message'] = 'URL must start with http://';
+    }
+
+    return $result;
+}
+
+if ($argc == 2) {
+    $url = $argv[1];
+    if(substr($url, -4, 4) == '.csv')
+    {
+        // Get the number of lines:
+        $linecount = 0;
+        $currentLine = 0;
+        $file = fopen($url, "r");
+        while(!feof($file)){
+          $line = fgets($file);
+          $linecount++;
+        }
+        fclose($file);
+
+        // Read it all:
+        $file = fopen($url, 'r');
+        $csv  = array(
+            array("url","success","message","ip","www","time_first","time_total","code","size")
+        );
+        echo "Checking " . ($linecount - 1) . " URL's: \n";
+        while($line = fgetcsv($file))
+        {
+            if($line[0] != 'site') {
+                $result = check_url($line[0]);
+                $csv[] = array_values($result);
+            }
+            $currentLine ++;
+            $p = round(($currentLine / $linecount) * 100);
+            echo $p . "%   \r";
+        }
+        echo "Done!   \n";
+        fclose($file);
+        $file = fopen('result.csv', 'w');
+        foreach($csv as $line) {
+            fputcsv($file, $line);
+        }
+        fclose($file);
+    } else {
+        $result = check_url($url);
+        print_r($result);
+    }
+} else {
+    echo "Usage:
+
+php ping.php [url]
+php ping.php [csv]
+\n";
+}
